e6d5656e8a4928f4b554ceddfbdeb8dede44e57b
lztanx152 <2423718052@qq.com>
2026-01-26 23:21:09 +0800

    第2章0126

---
+++ Chapter2/practice.c
@@ -0,0 +1,29 @@
+//
+// Created by root on 2026/1/25.
+//
+
+// 2.13 start
+int bis(int x, int m)
+{
+    return x | m;
+}
+
+int bic(int x, int m)
+{
+    return x & ~ m;
+}
+
+int bool_or(int x, int y) {
+    int result =bis(x,y);
+    return result;
+}
+int bool_xor(int x, int y)
+{
+    int result= bis(bic(x,y), bic(y,x));
+    return result;
+}
+// 2.13 end
+int main()
+{
+
+}
